18e242fa8467220e644968c956e7f0ab34f8a6f8
srbernard98 <bern.rojas@duocuc.cl>
2024-09-08 23:09:28 -0300

    update

--- src/app/fundfloe/fundfloe.page.ts
+++ src/app/fundfloe/fundfloe.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fundfloe',
   templateUrl: './fundfloe.page.html',
   styleUrls: ['./fundfloe.page.scss'],
 })
-export class FundfloePage implements OnInit {
+export class FundfloePage {
+  username: string = 'Usuario'; // Por defecto, cambia esto según la lógica de tu app
+  icons = [
+    { name: 'home', title: 'Inicio' },
+    { name: 'settings', title: 'Configuración' },
+    { name: 'person', title: 'Perfil' },
+    { name: 'notifications', title: 'Notificaciones' },
+    { name: 'help-circle', title: 'Ayuda' },
+    { name: 'information-circle', title: 'Información' }
+  ];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  logout() {
+    this.router.navigate(['/login']);
   }
-
 }
